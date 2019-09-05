f56c09a3f662c0ef656e611a2f5ed4933d0815c2
Alexander.J.Meyer <alexander.j.meyer@target.com>
2019-09-04 21:50:15 -0500

    home screen box with navigation

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,41 +1,47 @@
 import React from 'react';
-import Fade from 'react-reveal/Fade';
+import Card from '@material-ui/core/Card';
+import { makeStyles } from '@material-ui/core/styles';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 425,
+    backgroundColor: 'rgba(255, 255, 255, 1)',
+  },
+});
 
 const Home = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <Fade left>
-        <h1>Hello from the Home Page</h1>
-      </Fade>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <Fade right>
-        <h1>Some other text</h1>
-      </Fade>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <Fade left>
-        <h1>one more area of text</h1>
-      </Fade>
+    <div
+      style={{
+        position: 'absolute',
+        left: '50%',
+        top: '50%',
+        transform: 'translate(-50%, -50%)',
+      }}
+    >
+      <Card className={classes.card}>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Hello
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Welcome to my website!
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" color="primary" href="/resume">
+            Interactive Resume
+          </Button>
+          <Button size="small" color="primary" href="/contact">
+            Contact Me
+          </Button>
+        </CardActions>
+      </Card>
     </div>
   );
 };
--- src/views/Resume.jsx
+++ src/views/Resume.jsx
@@ -1,11 +1,46 @@
 import React from 'react';
+import Fade from 'react-reveal/Fade';
 
 const Resume = () => {
   return (
-    <h1>
-      Interactive resume / timeline will go here (potentially use react-reveal
-      for scroll animation)
-    </h1>
+    <div>
+      <h1>
+        Interactive resume / timeline will go here (potentially use react-reveal
+        for scroll animation)
+      </h1>
+      <Fade left>
+        <h1>Hello Resume Page</h1>
+      </Fade>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Fade right>
+        <h1>Some other text</h1>
+      </Fade>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Fade left>
+        <h1>one more area of text</h1>
+      </Fade>
+    </div>
   );
 };
 
