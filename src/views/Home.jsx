e6be4c4c18c2289002fc54cf57ab081e0d13b62e
Alexander.J.Meyer <alexander.j.meyer@target.com>
2019-09-09 22:34:04 -0500

    working start of home page with background image

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,42 +2,56 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import { makeStyles } from '@material-ui/core/styles';
 import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import BackgroundImage from '../assets/background_image.jpg';
 
 const useStyles = makeStyles({
   card: {
-    maxWidth: 425,
+    maxWidth: 600,
+    maxHeight: 600,
     backgroundColor: 'rgba(255, 255, 255, 1)',
+    position: 'absolute',
+    left: '50%',
+    top: '50%',
+    transform: 'translate(-50%, -50%)',
+  },
+  button: {
+    color: 'black',
+  },
+  bg: {
+    height: '100%',
+    width: '100%',
+    backgroundColor: 'black',
+    position: 'fixed',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center center',
+    backgroundRepeat: 'no-repeat',
+    backgroundAttachment: 'fixed',
+    backgroundImage: `url(${BackgroundImage})`,
+  },
+  bgImg: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    margin: 'auto',
+    width: '100%',
+    height: '100%',
   },
 });
 
 const Home = () => {
   const classes = useStyles();
   return (
-    <div
-      style={{
-        position: 'absolute',
-        left: '50%',
-        top: '50%',
-        transform: 'translate(-50%, -50%)',
-      }}
-    >
+    <div className={classes.bg}>
+      {/* <img src={NightSky} alt="bg"/> */}
       <Card className={classes.card}>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Hello
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Welcome to my website!
-          </Typography>
-        </CardContent>
         <CardActions>
-          <Button size="small" color="primary" href="/resume">
+          <Button size="small" className={classes.button} href="/resume">
             Interactive Resume
           </Button>
-          <Button size="small" color="primary" href="/contact">
+          <Button size="small" className={classes.button} href="/contact">
             Contact Me
           </Button>
         </CardActions>
