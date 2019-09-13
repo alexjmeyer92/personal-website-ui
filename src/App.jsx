e4be7d87e25c2cf54517ffffe60d5c4f677b35ed
Alexander.J.Meyer <alexander.j.meyer@target.com>
2019-09-12 21:32:33 -0500

    add global context to app

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,21 @@ import Home from './views/Home';
 import './styles/style.css';
 import Resume from './views/Resume';
 import Contact from './views/Contact';
+import GlobalContext from './components/Context/GlobalContext';
 
 const App = () => {
+  const [contextValues, setContextValues] = React.useState({
+    resumeSelection: null,
+  });
+
   return (
-    <Router primary={false}>
-      <Home path="/" />
-      <Resume path="/resume" />
-      <Contact path="/contact" />
-    </Router>
+    <GlobalContext.Provider vlaue={{ contextValues, setContextValues }}>
+      <Router primary={false}>
+        <Home path="/" />
+        <Resume path="/resume" />
+        <Contact path="/contact" />
+      </Router>
+    </GlobalContext.Provider>
   );
 };
 
--- src/views/Resume.jsx
+++ src/views/Resume.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
+import makeStyles from '@material-ui/core/styles/makeStyles';
 import ResumeItems from '../components/Resume/ResumeItems';
 import Layout from '../components/Layout/Layout';
+import BackgroundImage from '../assets/background_image.jpg';
+
+const useStyles = makeStyles({
+  bg: {
+    height: '100%',
+    width: '100%',
+    backgroundColor: 'black',
+    position: 'fixed',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center center',
+    backgroundRepeat: 'no-repeat',
+    backgroundAttachment: 'fixed',
+    backgroundImage: `url(${BackgroundImage})`,
+  },
+});
 
 const Resume = () => {
+  const classes = useStyles();
+
   return (
     <Layout>
-      <ResumeItems />
-      <h1>
-        Conditionally render resume content based on what is clicked above
-      </h1>
+      <div className={classes.bg}>
+        <ResumeItems />
+        <h1 style={{ color: 'white' }}>
+          Conditionally render resume content based on what is clicked above
+        </h1>
+      </div>
     </Layout>
   );
 };
