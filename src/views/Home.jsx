a3f01bfc1582aef1cf2502bb4a512f48a4301442
Alex Meyer <alexmeyer@Alex-MacBook.local>
2019-10-27 13:52:44 -0500

    update logos and background

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import { makeStyles } from '@material-ui/core/styles';
 import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
-import BackgroundImage from '../assets/background_image.jpg';
+import BackgroundImage from '../assets/personal_site_background.svg';
+import PersonalLogo from '../assets/box_with_arrow_logo.svg';
+import { CardMedia } from '@material-ui/core';
 
 const useStyles = makeStyles({
   card: {
-    maxWidth: 600,
+    maxWidth: 350,
     maxHeight: 600,
     backgroundColor: 'rgba(255, 255, 255, 1)',
     position: 'absolute',
@@ -21,7 +24,7 @@ const useStyles = makeStyles({
   bg: {
     height: '100%',
     width: '100%',
-    backgroundColor: 'black',
+    backgroundColor: 'white',
     position: 'fixed',
     backgroundSize: 'cover',
     backgroundPosition: 'center center',
@@ -37,6 +40,13 @@ const Home = () => {
     <div className={classes.bg}>
       {/* <img src={NightSky} alt="bg"/> */}
       <Card className={classes.card}>
+        <CardMedia>
+          <img src={PersonalLogo} className={classes.logo} alt="Alex Meyer Logo"/>
+        </CardMedia>
+        <CardContent>
+          My name is Alex Meyer. I am a product manager and sofware developer with experience creating data driven software products that drive efficiency and innovation across the 
+          supply chain space.          
+        </CardContent>
         <CardActions>
           <Button size="small" className={classes.button} href="/resume">
             Interactive Resume
--- src/views/Resume.jsx
+++ src/views/Resume.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import Layout from '../components/Layout/Layout';
-import BackgroundImage from '../assets/background_image.jpg';
 import ResumeBody from '../components/Resume/ResumeBody';
 
 const useStyles = makeStyles({
   bg: {
     height: '100%',
     width: '100%',
-    backgroundColor: 'black',
+    backgroundColor: 'white',
     position: 'fixed',
     backgroundSize: 'cover',
     backgroundPosition: 'center center',
     backgroundRepeat: 'no-repeat',
     backgroundAttachment: 'fixed',
-    backgroundImage: `url(${BackgroundImage})`,
   },
 });
 
