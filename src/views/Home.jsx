a4a220f53915688430fa4fcdc6f1e35eeea3b6b7
Alexander.J.Meyer <alexander.j.meyer@target.com>
2020-02-18 22:16:36 -0600

    testing home layout updates

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Fade from '@material-ui/core/Fade';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
-import { Button, Grid } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import Layout from '../components/Layout/Layout';
 import CenterLogo from '../assets/Main_Logo.svg';
 import ContactCard from '../components/Contact/ContactCard';
@@ -28,10 +28,6 @@ const Home = () => {
   const classes = useStyles();
 
   const [open, setOpen] = React.useState(false);
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
 
   const handleClose = () => {
     setOpen(false);
@@ -70,28 +66,65 @@ const Home = () => {
             alt="Home Page Center Logo"
           />
         </Grid>
-        <Grid item justify="space-between" xs={12}>
-          <img
-            style={{ height: '80px', width: '80px', padding: '15px' }}
-            src={Construction1}
-            alt="Construction Woker Logo"
-          />
-          <img
-            style={{ height: '80px', width: '80px', padding: '15px' }}
-            src={Construction1}
-            alt="Construction Woker Logo"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <h2>
-            Under Construction... Click contact me below if you are interested
-            in my resume or working with me
-          </h2>
-        </Grid>
-        <Grid item xs={12}>
-          <Button variant="contained" color="primary" onClick={handleOpen}>
-            Contact Me
-          </Button>
+        <br />
+        <Grid
+          container
+          spacing={5}
+          direction="row"
+          justify="center"
+          alignItems="center"
+        >
+          <Grid item xs={12} md={4}>
+            <img
+              style={{ height: '80px', width: '80px', padding: '15px' }}
+              src={Construction1}
+              alt="Construction Woker Logo"
+            />
+          </Grid>
+          <Grid item xs={12} md={8}>
+            <h2> Who Am I?</h2>
+            <h2>
+              {' '}
+              I am a product leader with experience designing, building, and
+              scaling products that solve the most complex problems at forture
+              30 companies. I am excited by buidling high performing teams with
+              diverse background and capabilities rooted in collaboration,
+              domain expertise, and deep technical accumen.
+            </h2>
+            <h2>
+              I also love bicycles, hiking, and chasing my 2 year old around the
+              house
+            </h2>
+          </Grid>
+          <Grid item xs={12} md={8}>
+            <h2>
+              {' '}
+              Overview of my work and skills will go here. I will talk about the
+              things I have built and the skills that I have.
+            </h2>
+            <h2>
+              {' '}
+              There will also be a button here linking to my resume / cv page.
+              Additionally include a button to contact / work with me
+            </h2>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <img
+              style={{ height: '80px', width: '80px', padding: '15px' }}
+              src={Construction1}
+              alt="Construction Woker Logo"
+            />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <img
+              style={{ height: '80px', width: '80px', padding: '15px' }}
+              src={Construction1}
+              alt="Construction Woker Logo"
+            />
+          </Grid>
+          <Grid item xs={12} md={8}>
+            <h2> Description of my blog section + links</h2>
+          </Grid>
         </Grid>
       </Grid>
     </Layout>
